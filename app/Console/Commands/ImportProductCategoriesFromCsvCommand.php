801c09994e7f0346b71db5a77727b4a9074dc4d4
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-02-15 21:06:44 +0100

    Implemented script to migrate data from CSV file into database

---
+++ app/Console/Commands/ImportProductCategoriesFromCsvCommand.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\ProductStatusEnum;
+use App\Models\Category;
+use App\Models\Department;
+use App\Models\Manufacturer;
+use App\Models\Product;
+use Illuminate\Console\Command;
+use League\Csv\Reader;
+
+class ImportProductCategoriesFromCsvCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:product-categories {file_path}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to import data from csv file into database';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $filePath = $this->argument('file_path');
+
+        if (!file_exists($filePath)) {
+            $this->error("File with path $filePath does not exist");
+            return 1;
+        }
+
+        $csvFile = Reader::createFromPath($filePath, 'r');
+        $csvFile->setHeaderOffset(0);
+
+        $records = $csvFile->getRecords();
+
+        foreach ($records as $offset => $record) {
+            $categoryName     = $record['category_name'];
+
+            //CSV has typo deparment instead of department
+            $departmentName   = $record['deparment_name'] ?? '';
+            $manufacturerName = $record['manufacturer_name'];
+            $productNumber    = $record['product_number'];
+            $sku              = $record['sku'];
+            $upc              = $record['upc'];
+            $regularPrice     = $record['regular_price'];
+            $salePrice        = $record['sale_price'];
+
+            $category = Category::firstOrCreate([
+                'category_name' => $categoryName
+            ]);
+
+            $department = Department::firstOrCreate([
+                'department_name' => $departmentName
+            ]);
+
+            $manufacturer = Manufacturer::firstOrCreate([
+                'manufacturer_name' => $manufacturerName
+            ]);
+
+            Product::create([
+                'category_id'    => $category->category_id,
+                'department_id'  => $department->department_id,
+                'manufacturer_id'=> $manufacturer->manufacturer_id,
+                'product_number' => $productNumber,
+                'sku'            => $sku,
+                'upc'            => $upc,
+                'regular_price'  => $regularPrice,
+                'sale_price'     => $salePrice,
+                'status'         => ProductStatusEnum::ACTIVE->value
+            ]);
+        }
+
+        $this->info("Import završen!");
+        return 0;
+    }
+}
---
+++ app/Enums/ProductStatusEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum ProductStatusEnum: string
+{
+    case ACTIVE = 'active';
+    case DELETED = 'deleted';
+}
