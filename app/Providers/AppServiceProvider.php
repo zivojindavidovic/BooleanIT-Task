907cc35ab2207583194b4438b821ce71c02c61c1
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-02-16 11:53:14 +0100

    Added binding for category service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\CategoryService;
+use App\Services\CategoryServiceImpl;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CategoryService::class, CategoryServiceImpl::class);
     }
 
     /**
