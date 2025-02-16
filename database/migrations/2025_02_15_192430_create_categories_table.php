30ff956f50a0759455c076c44a8f754adce7b656
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-02-16 11:49:47 +0100

    Added category field related to status / soft delete

--- database/migrations/2025_02_15_192430_create_categories_table.php
+++ database/migrations/2025_02_15_192430_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id('category_id');
             $table->string('category_name');
+            $table->enum('status', ['active', 'deleted'])->default('active');
             $table->timestamps();
         });
     }
