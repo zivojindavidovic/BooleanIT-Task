a2b9a7f181ce501c281164d60035853f0a956210
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-02-16 13:56:17 +0100

    Added offset functions

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\StatusEnum;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
@@ -16,5 +17,28 @@ class Category extends Model
     public function products()
     {
         return $this->hasMany(Product::class, 'category_id', 'category_id');
+    }
+
+    public function isDeleted(): bool
+    {
+        return $this->status == StatusEnum::DELETED->value;
+    }
+
+    public function activeProducts(): int
+    {
+        return $this->products()
+            ->where('status', StatusEnum::ACTIVE->value)
+            ->count() ?? 0;
+    }
+
+    public function offsetGet($offset): mixed
+    {
+        $camelCaseOffset = lcfirst(str_replace('_', '', ucwords($offset, '_')));
+
+        if (method_exists($this, $camelCaseOffset)) {
+            return $this->$camelCaseOffset();
+        }
+
+        return parent::offsetGet($offset);
     }
 }
